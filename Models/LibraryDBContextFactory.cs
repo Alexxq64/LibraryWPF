01ad1d3a82904c4352b2ebc6cfb3ca516930c2a7
Alexxq64 <alexxq@list.ru>
2025-05-01 09:52:55 +0300

    added DBTools

---
+++ Models/LibraryDBContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using LibraryWPF.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+public class LibraryDBContextFactory : IDesignTimeDbContextFactory<LibraryDBContext>
+{
+    public LibraryDBContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<LibraryDBContext>();
+        optionsBuilder.UseSqlServer("Server=.;Database=Library;Trusted_Connection=True;TrustServerCertificate=True;");
+
+        return new LibraryDBContext(optionsBuilder.Options);
+    }
+}
---
+++ Services/DBTools.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace LibraryWPF.Services
+{
+    public static class DBTools
+    {
+        private static string _dbName = "LibraryDB";
+
+        public static string DBName
+        {
+            get => _dbName;
+            set => _dbName = string.IsNullOrWhiteSpace(value)
+                ? throw new ArgumentException("Имя БД не может быть пустым")
+                : value;
+        }
+
+        public static string ConnectionString =>
+            $"Server=.;Database={DBName};Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public static SqlConnection CreateConnection() => new SqlConnection(ConnectionString);
+
+        public static bool TestConnection(string dbName)
+        {
+            try
+            {
+                using (var testConnection = new SqlConnection(
+                    $"Server=.;Database={dbName};Trusted_Connection=True;TrustServerCertificate=True;"))
+                {
+                    testConnection.Open();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
